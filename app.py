bf1a047ea8d626720c935faea47c7a6d99db2373
khamidou <khamidou@gmail.com>
2010-07-28 15:05:16 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+from Tkinter import *
+from scales import *
+
+current_scale = "C"
+current_mode = "C"
+
+class ScalesFrame(Frame):
+	def __init__(self, master=None, app=None):
+		Frame.__init__(self, master)
+		self.app = app
+		self.grid()
+		self.createWidgets()
+	
+	def closure(self, text):
+		def handler():
+			current_scale = text
+			s = Scale(scale=current_scale, mode=current_mode)
+			self.app.outputLabel.configure(text=str(s))
+		return handler
+
+	def createWidgets(self):
+		for i, button in enumerate(scales):
+			self.button = Radiobutton(self, text=str(button), command=self.closure(str(button)))
+			self.button.deselect()
+			self.button.grid(row=0, column=i)
+
+class ModeFrame(Frame):
+	def __init__(self, master=None, app=None):
+		Frame.__init__(self, master)
+		self.app = app
+		self.grid()
+		self.createWidgets()
+	
+	def closure(self, text):
+		def handler():
+			current_mode = modes[text]
+			s = Scale(scale=current_scale, mode=current_mode)
+			self.app.outputLabel.configure(text=str(s))
+		return handler
+
+
+	def createWidgets(self):
+		for i, button in enumerate(modes):
+			self.button = Radiobutton(self, text=str(button), command=self.closure(str(button)))
+			self.button.deselect()
+			self.button.grid(row=0, column=i)
+
+
+class Application(Frame):
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+	
+	def createWidgets(self):
+		self.outputLabel = Label(self, text="Placeholder")
+
+		self.scalesLabel = LabelFrame(self, text="Scale")
+		self.scalesLabel.grid()
+		self.scalesFrame = ScalesFrame(master=self.scalesLabel, app=self)
+
+		self.modesLabel = LabelFrame(self, text="Modes")
+		self.modesLabel.grid()
+		self.modesFrame = ModeFrame(master=self.modesLabel, app=self)
+
+
+		self.outputLabel.grid()
+
+		self.quitButton = Button(self, text="Quit", command=self.quit)
+		self.quitButton.grid()
+
+if __name__ == "__main__":
+	root = Tk()
+	app = Application(master=root)
+	app.master.title("Scaletool")
+	app.mainloop()
---
+++ scales.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import itertools
+
+# A ring class - we use it to transpose our basic scale : C D E F G A B
+class Ring:
+    def __init__(self, l):
+        if not len(l):
+            raise "ring must have at least one element"
+        self._data = l
+
+    def __repr__(self):
+        return repr(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, i):
+    	if i >= len(self._data):
+		i = i % len(self._data)
+
+        return self._data[i]
+    def __delitem__(self, i):
+    	del self._data[i]
+
+    def index(self, i):
+    	return self._data.index(i)
+
+    def turn(self):
+    	# Scales turn in reverse-order
+        first = self._data.pop(0)
+        self._data.append(first)
+
+    def first(self):
+        return self._data[0]
+
+    def last(self):
+        return self._data[-1]
+
+    def turn_until(self, value):
+    	while self.first() != value:
+		self.turn()
+
+    def turn_ntimes(self, times):
+    	i = 0
+    	while i < times:
+		self.turn()
+		i += 1
+
+scales = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+modes = {"Ionian" : "C", "Dorian" : "D", "Phrygian" : "E", "Lydian" : "F",
+	 "Mixolydian" : "G", "Aeolian" : "A", "Locrian" : "B"}
+
+class Scale:
+	def __init__(self, scale="C", mode="C"):
+		self.start = Ring(["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"])
+		# We store a modified scale as numbers : 1 means no sharp or flat, 0.5, one flat, 1.5 one sharp
+		self.values = Ring([2, 2, 1, 2, 2, 2, 1])
+		self.transpose_scale(scale)
+		self.change_mode(mode)
+	
+	def transpose_scale(self, start_letter):
+		self.start.turn_until(start_letter)
+
+	def change_mode(self, mode_letter):
+		v = filter(lambda x: "#" not in x, self.start._data)
+
+		self.values.turn_ntimes(v.index(mode_letter))
+
+	def __str__(self):
+		print self.values
+		retscale = self.start[0] + " " 
+		notes = self.start
+		translation_table = {	"Cb": "B",
+					"Db" : "C#",
+					"Eb" : "D#",
+					"Fb" : "E",
+					"Gb" : "F#",
+					"Ab" : "G#",
+					"Bb" : "A#", 
+					"B#" : "C",
+					"E#" : "F",
+					}
+
+		i = 0
+		j = 0
+		while i < 11 and j < 6: # reduce(lambda x,y: x+y, self.values)
+			note = notes[i]
+			if self.values[j] == 1:
+				retscale += notes[i+1]
+				i += 1 
+			elif self.values[j] == 2:	
+				retscale += notes[i+2]
+				i += 2 
+
+			j += 1
+			retscale += " "
+		return retscale
+
